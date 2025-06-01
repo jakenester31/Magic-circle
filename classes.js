a5326d711a9337e2bdd1124978691af83d33fb7c
jakenester31 <111662560+jakenester31@users.noreply.github.com>
2025-05-31 18:45:37 -0700

    masive update
    - restarted everything (give or take)
    - lazy inc updated to look same as expensive inc
    - Magic circle obj act like groups
    - shape, arc objs in magic circles
    - etc...

---
+++ classes.js
@@ -0,0 +1,185 @@
+//@ Classes
+class magic_circle {
+    #visible = 1;
+    #scale = 1;
+    constructor(x,y,scale) {
+        Object.assign(this,{x,y,...drawSettings});
+        this.children = [];
+        obj.push(this);
+        obj.last = this;
+        this.#scale = scale || 1;
+    }
+    draw() {
+        if (this.#visible == 0){
+            return(0);
+        }
+        for (var i = 0; i < this.children.length; i++){
+            this.children[i].draw();
+        }
+    }
+
+    set add(val) { // OMG where has this been my whole life? Its like a function but it looks like setting a variable??
+        this.children.push(val);
+        return val;
+    }
+
+    set visible(val){
+        if ([0,1].includes(val)){
+            this.#visible = val;
+            return(val);
+        }
+        if (typeof val !== 'number'){
+            throw new TypeError('Input must be a number');
+        }
+        throw new RangeError('Input must be 0 or 1');
+    }
+    // scale
+    get scale() { return(this.#scale); }
+
+    set scale(val) {
+        if (typeof val != 'number') {
+            throw TypeError('Scale must be a number');
+        }
+        this.#scale = val;
+        return(this.#scale);
+    }
+}
+
+class shape {
+    #incAngle;
+    #offset = 0;
+    #scale = 'inherit';
+    #scaleStyle = 'inherit';
+    constructor(x,y,sides,radius,offAngle,incAngle) {
+        let parent = Object.fromEntries( Object.entries(obj.last).filter(e => Object.keys(drawSettings).includes(e[0])) );
+        Object.assign(this,{x,y,sides,radius,offAngle,...parent});
+        // Record parent child relationship
+        obj.last.add = this;
+        this.parent = obj.last;
+        this.parent.last = this;
+        // Default angle values
+        this.offAngle = offAngle || 0;
+        this.#incAngle = incAngle || 0;
+    }
+    draw() {
+        DrawSetup(this);
+        let a = 360 / this.sides;
+        context.beginPath();
+        for (var i = 0; i < this.sides; i++) {
+            let fx = (this.#offset + angle * -this.incAngle - this.offAngle + a * i) * PI;
+            context.lineTo(
+                this.parent.x + this.x + this.radius * -Math.sin(fx) * this.scale
+                ,this.parent.y + this.y + this.radius * -Math.cos(fx) * this.scale
+            );
+        }
+        context.closePath();
+        context.stroke();
+    }
+    // increment angle
+    get incAngle() { return(this.#incAngle); }
+
+    set incAngle(val) {
+        if (typeof val !== 'number')
+            throw TypeError('Angle increment must be a number');
+        this.#offset += angle * (val - this.#incAngle)
+        this.#incAngle = val;
+    }
+    // Scale & scale style
+    get scale() {
+        if (this.#scale == 'inherit'){
+            return(this.parent.scale);
+        }
+        if (this.#scaleStyle == 'overRide')
+            return(this.#scale);
+        return(this.#scale * this.parent.scale);
+    }
+
+    set scale(val) {
+        if (typeof val != 'number' && val != 'inherit') {
+            throw TypeError('Scale must be a number or "inherit"');
+        }
+        this.#scale = val;
+        return(this.#scale);
+    }
+
+    get scaleStyle(){ return(this.#scaleStyle); }
+
+    set scaleStyle(val){
+        if (!['inherit','overRide'].includes(val)){
+            throw TypeError('ScaleStyle must be "inherit" or "overRide"');
+        }
+        this.#scaleStyle = val;
+    }
+}
+
+class arc {
+    #offset = 0;
+    #incAngle;
+    #scale = 'inherit';
+    #scaleStyle = 'inherit';
+    constructor(x,y,radius,angle1,angle2,incAngle) {
+        let parent = Object.fromEntries( Object.entries(obj.last).filter(e => Object.keys(drawSettings).includes(e[0])) );
+        Object.assign(this,{x,y,radius,...parent});
+        // Record parent child relationship
+        obj.last.add = this;
+        this.parent = obj.last;
+        this.parent.last = this;
+        this.angle1 = angle1 || 0;
+        this.angle2 = angle2 || 2
+        this.#incAngle = incAngle || 0;
+    }
+    draw(){
+        DrawSetup(this);
+        context.beginPath();
+        context.arc(this.x + this.parent.x,this.y + this.parent.y, this.radius * this.scale,
+            (this.angle2 + this.#offset + angle * this.incAngle) * PI,
+            (this.angle1 + this.#offset + angle * this.incAngle) * PI);
+        context.stroke();
+        context.closePath();
+    }
+
+    // increment angle
+    get incAngle() { return(this.#incAngle); }
+
+    set incAngle(val) {
+        if (typeof val !== 'number')
+            throw TypeError('Angle increment must be a number');
+        this.#offset += angle * (val - this.#incAngle)
+        this.#incAngle = val;
+    }
+
+    // Scale & scale style
+    get scale() {
+        if (this.#scale == 'inherit'){
+            return(this.parent.scale);
+        }
+        if (this.#scaleStyle == 'overRide')
+            return(this.#scale);
+        return(this.#scale * this.parent.scale);
+    }
+
+    set scale(val) {
+        if (typeof val != 'number' && val != 'inherit') {
+            throw TypeError('Scale must be a number or "inherit"');
+        }
+        this.#scale = val;
+        return(this.#scale);
+    }
+
+    get scaleStyle(){ return(this.#scaleStyle); }
+
+    set scaleStyle(val){
+        if (!['inherit','overRide'].includes(val)){
+            throw TypeError('ScaleStyle must be "inherit" or "overRide"');
+        }
+        this.#scaleStyle = val;
+    }
+}
+
+//* Functions
+function DrawSetup(obj) {
+    context.strokeStyle = obj.color;
+    context.fillStyle = obj.color;
+    context.lineJoin = obj.lineJoin;
+    context.lineWidth = obj.lineWidth;
+}
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// General
+//* General
 const 
 doc = document.documentElement,
 canvas = document.querySelector('#workspace'),
@@ -8,11 +8,15 @@ workspace = {
     y:0,
     scale:1
 },
-settings = {
-    lazy:0,
+drawSettings = {
+    color:'purple',
+    lineWidth:3,
+    lineJoin:'miter',   // miter, bevel, round
 },
 obj = [],
-pi = Math.PI / 180;
+PI = Math.PI / 180;
+var angle = 0,
+mouse = {x:0,y:0};
 
 new ResizeObserver(function(){
     canvas.height = canvas.clientHeight;
@@ -21,135 +25,17 @@ new ResizeObserver(function(){
 }).observe(canvas);
 
 
-workspace.scale = 2.3;
-workspace.x = 50;
-workspace.y = 100;
+// Objects (testing)
+const t = new magic_circle(100,100,1);
+const a = new shape(0,0,3,120,0,-5);
+new shape(0,0,3,120,60,-5);
+new shape(0,0,3,50,0,5);
+new shape(0,0,3,50,60,5);
+const b = new arc(0,0,55,0,180,5);
+// obj.last.last.scale = 2;
 
 
-var angle = 0;
-
-class shape {
-    constructor(x,y,rad,sides,ang,incAng) {
-        Object.assign(this,{x:x,y:y,sides:sides,radius:rad,angle:{c:ang,i:incAng}})
-        this.lineWidth = drawSettings.lineWidth;
-        this.color = drawSettings.color;
-        this.lineJoin = drawSettings.lineJoin;
-        obj.push(this);
-    }
-    draw() {
-        let a = 360 / this.sides;
-        setObjDraw(this);
-        context.beginPath();
-        for(var i = 0; i < this.sides; i++){
-            if (settings.lazy != 1){
-                context.lineTo(offsetObj.x + this.x + this.radius * -Math.sin(-(this.angle.c + (a * i)) * pi),offsetObj.y + this.y + this.radius * -Math.cos(-(this.angle.c + (a * i)) * pi));
-            } else {
-                context.lineTo(this.x + this.radius * -Math.sin(-(angle * this.angle.i + this.angle.c + (a * i)) * pi),this.y + this.radius * -Math.cos(-(angle * this.angle.i + this.angle.c + (a * i)) * pi));
-            }
-        }
-        context.closePath();
-        context.stroke();
-    }
-}
-
-class circle {
-    constructor(x,y,rad) {
-        Object.assign(this,{x:x,y:y,radius:rad});
-        obj.push(this);
-    }
-    draw() {
-        setObjDraw(this);
-        context.beginPath();
-            context.arc(this.x,this.y,this.radius,0,2 * Math.PI);
-        context.stroke();
-        context.closePath();
-    }
-}
-
-class line {
-    constructor(x,y,point1,point2,incAng) {
-        Object.assign(this,{x:x,y:y,point1:point1,point2:point2,incAng:incAng});
-        obj.push(this);
-    }
-    draw() {
-        // console.log(this.point1);
-        setObjDraw(this);
-        context.beginPath();
-        if (settings.lazy != 1){
-            context.moveTo(this.x + this.point1[0] * -Math.sin(-this.point1[1] * pi),this.y + this.point1[0] * -Math.cos(-this.point1[1] * pi));
-            context.lineTo(this.x + this.point2[0] * -Math.sin(-this.point2[1] * pi),this.y + this.point2[0] * -Math.cos(-this.point2[1] * pi));
-        } else {
-            context.fillRect(this.x - 5,this.y - 5,10,10);
-            for (var i = 0; i < 2; i++){
-                let point = [this.point1,this.point2][i]
-                let rad = Math.sqrt(point[0] * point[0] + point[1] * point[1] )
-                let dir = Math.atan2(point[0],point[1]);
-                // console.log(dir / pi);
-                context.lineTo(this.x + rad * Math.sin(dir),this.y + rad * Math.cos(dir));
-            }
-        }
-        
-        context.stroke();
-        context.closePath();
-    }
-}
-
-class offset {
-    constructor(x,y,rad,ang,incAng) {
-        Object.assign(this,{x:x,y:y,radius:rad,angle:{c:ang,i:incAng}})
-        obj.push(this);
-    }
-    draw() {
-        offsetObj.x += this.x + this.radius * -Math.sin(-this.angle.c * pi);
-        offsetObj.y += this.y + this.radius * -Math.cos(-this.angle.c * pi);
-        if (this.x == 'reset'){
-            offsetObj.x = 0;
-        } 
-        if (this.y == 'reset'){
-            offsetObj.y = 0;
-        }
-    }
-}
-
-
-
-const drawSettings = {
-    color:'purple',
-    lineWidth:1,
-    lineJoin:'round',   // miter, bevel, round
-}
-
-// Objects
-
-// inner triangle
-drawSettings.lineJoin = 'bevel';
-new shape(100,100,30,3,0,-4);
-new shape(100,100,30,3,60,-4);
-new shape(100,100,30,6,60,-4);
-// outer
-new shape(100,100,60,3,0,4);
-new shape(100,100,60,3,60,4);
-new shape(100,100,60,6,60,4);
-new circle(100,100,60);
-
-// small details 1
-drawSettings.lineJoin = 'miter';
-for (var i = 0; i < 6; i++){
-    new line(100,100,[50,-3 + 60 * i],[40,-13 + 60 * i],4);
-    new line(100,100,[50,3 + 60 * i],[40,13 + 60 * i],4);
-    new line(100,100,[35,-11 + 60 * i],[35,11 + 60 * i],4);
-
-    new offset(0,0,41,60 * i,4);
-    new shape(100,100,7,3,0,-16);
-    new shape(100,100,7,3,60,-16);
-    new offset('reset','reset');
-}
-
-
-
-
-// draw
-update.pause = 1;
+//# draw
 setInterval(draw,30);
 function draw() {
     // Setup
@@ -157,41 +43,14 @@ function draw() {
     context.translate(workspace.x,workspace.y);
     context.scale(workspace.scale,workspace.scale);
     context.clearRect(-workspace.x / workspace.scale,-workspace.y / workspace.scale,doc.clientWidth / workspace.scale,doc.clientHeight / workspace.scale);
-    update();
-    // context.arc();
-}
-
-var offsetObj = {x:0,y:0};
-
-function update() {
-    offsetObj = {x:0,y:0};
-    settings.lazy == 1 && update.pause != 1 && (angle = ++angle % 360);
-    for(var i = 0; i < obj.length; i++){
-        if (settings.lazy != 1 && update.pause != 1){
-            ['shape','offset'].indexOf(obj[i].constructor.name) > -1 && (obj[i].angle.c += obj[i].angle.i);
-            if (obj[i].constructor.name === 'line'){
-                obj[i].point1[1]+= obj[i].incAng;
-                obj[i].point2[1]+= obj[i].incAng;
-            }
-        }
+    // update
+    draw.pause != 1 && (angle = ++angle % 360);
+    for (var i = 0; i < obj.length; i++){
         obj[i].draw();
     }
-    // console.log(offsetObj);
 }
 
-function setObjDraw(obj){
-    context.strokeStyle = obj.color;
-    context.fillStyle = obj.color;
-    context.lineJoin = obj.lineJoin;
-    context.lineWidth = obj.lineWidth;
-}
-
-
-
-
-
-// Translations
-
+//* Translations
 canvas.addEventListener('mousedown', e => {
     if (e.button !== 0){
         return(0);
@@ -203,10 +62,22 @@ canvas.addEventListener('mousedown', e => {
     function move(e) {
         mp.shift();
         mp.push([e.clientX,e.clientY]);
+
+        // mouse bounding
+        for (var i = 0; i < 2; i++){
+            const dim = canvas['client' + ['Width','Height'][i]];
+            mp[1][i] < 0 && (mp[1][i] = 0);
+            mp[1][i] > dim && (mp[1][i] = dim);
+        }
+
         workspace.x += mp[1][0] - mp[0][0];
         workspace.y += mp[1][1] - mp[0][1];
+
+        mouse = {x:mp[1][0],y:mp[1][1]}
     }
     function stop() {
+        mouse.x = undefined;
+        mouse.y = undefined;
         removeEventListener('mousemove',move);
         removeEventListener('mouseup',stop);
     }
@@ -220,7 +91,13 @@ canvas.addEventListener('wheel', e => {
     // old/new scales
     const old = workspace.scale; 
     workspace.scale += dir * workspace.scale / 10;
+    // Bind scale
+    workspace.scale > 20 && (workspace.scale = 20);
+    workspace.scale < 1/10 && (workspace.scale = 1/10);
+    // Which pos?
+    let x = mouse.x || e.clientX;
+    let y = mouse.y || e.clientY;
     // reposition workspace
-    workspace.x -= (e.clientX - workspace.x) / old * (workspace.scale - old);
-    workspace.y -= (e.clientY - workspace.y) / old * (workspace.scale - old);
+    workspace.x -= (x - workspace.x) / old * (workspace.scale - old);
+    workspace.y -= (y - workspace.y) / old * (workspace.scale - old);
 }, {passive:false})
